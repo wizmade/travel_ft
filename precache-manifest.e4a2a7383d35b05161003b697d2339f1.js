34e831e3759a5856efa1b0e35fdd8a143b1333ff
root <root@softer008.cafe24.com>
2020-09-11 11:49:17 +0900

    Updates

---
+++ precache-manifest.e4a2a7383d35b05161003b697d2339f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d480410b3c611a35760bade9dead3105",
+    "url": "/travel_ft/index.html"
+  },
+  {
+    "revision": "b61f82a3aa69e1e19de5",
+    "url": "/travel_ft/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "04b37803b988ac08e649",
+    "url": "/travel_ft/static/css/main.ee526b49.chunk.css"
+  },
+  {
+    "revision": "b61f82a3aa69e1e19de5",
+    "url": "/travel_ft/static/js/2.a0dd9dc8.chunk.js"
+  },
+  {
+    "revision": "317c91338e4f280e2fcc298d243a4287",
+    "url": "/travel_ft/static/js/2.a0dd9dc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04b37803b988ac08e649",
+    "url": "/travel_ft/static/js/main.1b161ce1.chunk.js"
+  },
+  {
+    "revision": "97db4ebac98f9c981e96",
+    "url": "/travel_ft/static/js/runtime-main.4d751659.js"
+  },
+  {
+    "revision": "36e1765e09280e2d0b576ae0265ebccc",
+    "url": "/travel_ft/static/media/bg.36e1765e.png"
+  }
+]);
